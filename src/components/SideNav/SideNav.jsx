668e4bc63a24397b58c011115758bc28775ddee8
Yoni <yoniakabecky@gmail.com>
2019-11-21 17:40:44 -0800

    Changed sideNav part
    imprement react-scroll

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import './SideNav.css';
+import { Link } from 'react-scroll';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
 function SideNav() {
   return (
     <div className="SideNav">
-      <div>
-        <a href="#" className="SideNav-link"></a>
-      </div>
-      <div>
-        <a href="#" className="SideNav-link"></a>
-      </div>
-      <div>
-        <a href="#" className="SideNav-link"></a>
-      </div>
-      <div>
-        <a href="#" className="SideNav-link"></a>
-      </div>
+      <Link activeClass="sideNavActive" className="SideNav-link" to="#home" spy={true} offset={-100}><FontAwesomeIcon icon={faCircle} /></Link>
+      <Link activeClass="sideNavActive" className="SideNav-link" to="#about" spy={true}><FontAwesomeIcon icon={faCircle} /></Link>
+      <Link activeClass="sideNavActive" className="SideNav-link" to="#contact" spy={true}><FontAwesomeIcon icon={faCircle} /></Link>
     </div>
   );
 }
